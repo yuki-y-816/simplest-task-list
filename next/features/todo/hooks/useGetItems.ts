1ff4b45302c7082d0e521e5e2706029c5c6e256d
Yuki_Yamashita <yyper816@gmail.com>
2023-07-17 09:57:20 +0900

    [add] todo タスク表示のフロント実装

---
+++ next/features/todo/hooks/useGetItems.ts
@@ -0,0 +1,34 @@
+import { ApiGateway } from "@/consts/app"
+import { type User } from "@/features/user/types"
+import type { TodoItems, Item } from "@/features/todo/types"
+
+type postData = {
+    method: "select"
+    todo: Item
+}
+
+export const useGetItems = async (userId: User["id"]): Promise<TodoItems> => {
+    let todoItems: TodoItems = []
+    const postData: postData = {
+        method: "select",
+        todo: {
+            userId: userId,
+        },
+    }
+
+    try {
+        const fetched = await fetch(`${ApiGateway}/todo`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(postData),
+        }).then((res) => res.json())
+
+        todoItems = fetched
+    } catch (error) {
+        console.log("error!! ->", error)
+    }
+
+    return todoItems
+}
+
+export default useGetItems
---
+++ next/features/todo/types/index.ts
@@ -0,0 +1,9 @@
+export type TodoItems = Array<Item>
+
+export type Item = {
+    id?: number
+    userId?: string
+    task?: string
+    createdAt?: string
+    updatedAt?: string
+}
